6b8c40ba35c081946d70096500042bce97249630
Eric-Pessoa <pessoa.eric@gmail.com>
2022-09-28 11:01:10 -0300

    chore: updated individual mines responsiveness

--- app.js
+++ app.js
@@ -2,24 +2,35 @@ window.onload = () => {
   calculateIndividualTile()
 };
 
+convertPixelToVw = (pixelWidth) => {
+  const screenWidth = screen.width;
+  return ((pixelWidth / screenWidth) * 100).toFixed(1)
+ }
+
+ convertPixelToVh = (pixelHeight) => {
+  const screenHeight = screen.height;
+  return ((pixelHeight / screenHeight) * 100).toFixed(1)
+ }
 
 calculateIndividualTile = () => {
   const container3 = document.getElementsByClassName("container_3");
 
-  const fatherElementWidth = container3[0].clientWidth;
-  const fatherElementHeight = container3[0].clientHeight;
+  const fatherElementVw = convertPixelToVw(container3[0].clientWidth);
+  const fatherElementVh = convertPixelToVh(container3[0].clientHeight);
 
-  const childrenElementWidth = fatherElementWidth / 3
-  const childrenElementHeight = fatherElementHeight / 3
+  const childrenElementVw = (fatherElementVw / 3)
+  const childrenElementVh = (fatherElementVh / 3)
+
+  console.log(fatherElementVw)
+  console.log(fatherElementVh)
+  console.log(childrenElementVw)
+  console.log(childrenElementVh)
 
   let single_cell = document.getElementsByClassName("single_cell");
 
 
   for (let i = 0; i < single_cell.length; i++) {
-    console.log(i)
-    console.log(childrenElementHeight)
-    single_cell[i].style.width = `${childrenElementWidth}px`
-    single_cell[i].style.height = `${childrenElementHeight}px`
+    single_cell[i].style.width = `${childrenElementVw}vw`
+    single_cell[i].style.height = `${childrenElementVh}vh`
  }
-
 }
